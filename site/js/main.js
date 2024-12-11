6c854bddf1530ef507bcd6cc300c9959db828e95
chikchicly <chikchicly@gmail.com>
2024-12-11 16:48:00 +0700

    google tag

---
+++ site/js/main.js
@@ -0,0 +1,4 @@
+document.querySelector('.center > div:nth-child(1) > button:nth-child(3)').addEventListener('click',()=>{
+    window.location.href=`https://wa.me/79528885656?text=Hi!%20%F0%9F%91%8B%20I'm%20interested%20...`;
+    // window.location.href=`https://wa.clck.bar/79528885656?text=Hi,%20%F0%9F%91%8B%20I'm%20interested%20...`;
+})
