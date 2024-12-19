5d4aa3b04f891d4d30440dea2a7805e0fd4b5cca
chikchicly <chikchicly@gmail.com>
2024-12-19 12:16:00 +0500

    news css and menuFooter

--- site/js/menuFooter.js
+++ site/js/menuFooter.js
@@ -1,5 +1,11 @@
 function mFooter(){
+    document.querySelector('.menuFooter').addEventListener('click',()=>{
+        const mFooter=document.createElement('div');
+        mFooter.classList.add('mFooter');
+        document.querySelector('body').append(mFooter);
+    });
+    
     return console.log('It\'s menu in Footer');
-}
 
+}
 mFooter();
