e5764fcb3ab7b636be257db22e3cace6875a4730
chikchicly <chikchicly@gmail.com>
2024-12-20 13:31:52 +0500

    O_o

--- site/js/menuFooter.js
+++ site/js/menuFooter.js
@@ -3,6 +3,13 @@ function mFooter(){
         const mFooter=document.createElement('div');
         mFooter.classList.add('mFooter');
         document.querySelector('body').append(mFooter);
+
+        setTimeout(()=>{
+            document.querySelector('.mFooter').style.cssText=`
+            height:100vh;
+            opacity:1;
+            `;
+        },100);
     });
     
     return console.log('It\'s menu in Footer');
