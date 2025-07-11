aad5393796d85ed7a5143f2ebcc20fce5703e624
chikchicly <chikchicly@gmail.com>
2025-07-11 10:05:01 +0700

    mFooter

--- site/js/menuFooter.js
+++ site/js/menuFooter.js
@@ -3,16 +3,25 @@ function mFooter(){
         const mFooter=document.createElement('div');
         mFooter.classList.add('mFooter');
         document.querySelector('body').append(mFooter);
-
+        mFooter.innerHTML=`${menuList}`;
         setTimeout(()=>{
             document.querySelector('.mFooter').style.cssText=`
-            height:100vh;
-            opacity:1;
+                height:100vh;
+                opacity:1;
             `;
+
+            document.querySelector(".mFooter").style.fontSize="33px";
+           
         },100);
+        document.querySelector(".mFooter").addEventListener("click",()=>{
+            document.querySelector(".mFooter").style.height="0";
+            setTimeout(()=>{
+                document.querySelector('.mFooter').remove();
+            },100);
+        })
     });
     
-    return console.log('It\'s menu in Footer');
-
+    // return console.log('It\'s menu in Footer '+ menuList);
+    
 }
 mFooter();
